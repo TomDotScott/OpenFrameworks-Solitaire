0c8ed41617c36328936667ba5d0234e773eb8549
TomDotScott <tom.scott45456@gmail.com>
2021-02-12 18:38:19 +0000

    Playing Card class

---
+++ Solitaire/src/PlayingCard.cpp
@@ -0,0 +1,85 @@
+﻿#include "PlayingCard.h"
+
+#include "Constants.h"
+
+PlayingCard::PlayingCard(const eSuit suit, const eValue value) :
+	m_suit(suit),
+	m_value(value)
+{
+	m_position.x = constants::screenWidth - constants::cardWidth - constants::outlineWidth;
+	m_position.y = constants::outlineWidth;
+}
+
+ofColor PlayingCard::GetColour() const
+{
+	switch (m_suit)
+	{
+	case eSuit::e_Hearts:
+	case eSuit::e_Diamonds:
+		return { 255, 0, 0 };
+		break;
+	case eSuit::e_Clubs:
+	case eSuit::e_Spades:
+		return { 0, 0, 0 };
+		break;
+	}
+
+	return {};
+}
+
+ofVec2f PlayingCard::GetPosition() const
+{
+	return m_position;
+}
+
+void PlayingCard::SetPosition(const ofVec2f& vec)
+{
+	m_position = vec;
+}
+
+std::string PlayingCard::GetValue() const
+{
+	switch (m_value)
+	{
+		case eValue::e_Ace:
+			return "A";
+			break;
+		case eValue::e_Two: 
+			return "2";
+			break;
+		case eValue::e_Three: 
+			return "3";
+			break;
+		case eValue::e_Four: 
+			return "4";
+			break;
+		case eValue::e_Five: 
+			return "5";
+			break;
+		case eValue::e_Six: 
+			return "6";
+			break;
+		case eValue::e_Seven: 
+			return "7";
+			break;
+		case eValue::e_Eight: 
+			return "8";
+			break;
+		case eValue::e_Nine: 
+			return "9";
+			break;
+		case eValue::e_Ten: 
+			return "10";
+			break;
+		case eValue::e_Jack: 
+			return "J";
+			break;
+		case eValue::e_Queen: 
+			return "Q";
+			break;
+		case eValue::e_King: 
+			return "K";
+			break;
+	}
+	return "ERROR";
+}
---
+++ Solitaire/src/PlayingCard.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+#include <ofColor.h>
+#include <ostream>
+#include <ofVec2f.h>
+
+enum class eSuit
+{
+	e_Hearts,
+	e_Diamonds,
+	e_Clubs,
+	e_Spades
+};
+
+enum class eValue
+{
+	e_Ace,
+	e_Two,
+	e_Three,
+	e_Four,
+	e_Five,
+	e_Six,
+	e_Seven,
+	e_Eight,
+	e_Nine,
+	e_Ten,
+	e_Jack,
+	e_Queen,
+	e_King
+};
+
+class PlayingCard
+{
+public:
+	PlayingCard(eSuit suit, eValue value);
+	ofColor GetColour() const;
+
+	ofVec2f GetPosition() const;
+	void SetPosition(const ofVec2f& vec);
+
+	std::string GetValue() const;
+
+private:
+	eSuit m_suit;
+	eValue m_value;
+	ofVec2f m_position;
+};
